a63df51b909d5950b54f91c174b43bc685bbb0a7
mr-kamy <steam-punker@mail.ru>
2018-04-09 14:41:20 +0300

    error correction

--- App/Controllers/Admin.php
+++ App/Controllers/Admin.php
@@ -8,7 +8,7 @@ class Admin extends Controller
 {
     protected function handle()
     {
-        $this->view->news = \App\Models\Article::findLast(3);
+        $this->view->news = \App\Models\Article::findAll();
         echo $this->view->render(__DIR__ . '/../../templates/admin.php');
     }
 
--- App/Controllers/UpdateArticle.php
+++ App/Controllers/UpdateArticle.php
@@ -11,8 +11,7 @@ class UpdateArticle extends Controller
     protected function handle()
     {
         if ((!empty($_POST['title'])) && (!empty($_POST['content'])) && (!empty($_GET['id']))) {
-            $article = new Article();
-            $article->id = $_GET['id'];
+            $article = Article::findById($_GET['id']);
             $article->title = $_POST['title'];
             $article->content = $_POST['content'];
             $article->save();
