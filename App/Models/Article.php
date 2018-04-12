07d776417535fdc911884f23f59ac394a5ce52f0
mr-kamy <steam-punker@mail.ru>
2018-04-12 13:24:51 +0300

    error correction

--- App/Models/Article.php
+++ App/Models/Article.php
@@ -34,9 +34,9 @@ class Article extends Model
      */
     public function __get($name)
     {
-        if ('author' == $name) {;
-            if ((isset($this->author_id) && (!empty($this->author_id)))) {
-                return Author::findById($this->id)->name;
+        if ('author' == $name) {
+            if (!empty($this->author_id)) {
+                return Author::findById($this->author_id);
             }
         }
     }
