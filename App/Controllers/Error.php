448b43df36f1dc25e405f4903cdc96bcb3e182f6
mr-kamy <steam-punker@mail.ru>
2018-04-20 15:44:25 +0300

    swiftmailer

--- App/Controllers/Error.php
+++ App/Controllers/Error.php
@@ -10,24 +10,20 @@ class Error extends Controller
 
     protected function handle()
     {
-        if (100 == $this->code){
+        if (100 == $this->code) {
             $transport = (new \Swift_SmtpTransport('smtp.googlemail.com', 465, 'ssl'))
                 ->setUsername('mr.kamc@gmail.com')
-                ->setPassword('******')
-            ;
+                ->setPassword('******');
 
             $mailer = new \Swift_Mailer($transport);
-
 
             $message = (new \Swift_Message($this->message))
                 ->setFrom(['mr.kamc@gmail.com' => 'Алексей Калугин'])
                 ->setTo(['mr.kamc@gmail.com'])
-                ->setBody($this->message . ' код - ' . $this->code . ' время: ' . date('c'))
-            ;
+                ->setBody($this->message . ' код - ' . $this->code . ' время: ' . date('c'));
 
             $mailer->send($message);
         }
-
         $this->view->message = $this->message;
         $this->view->code = $this->code;
         echo $this->view->render(__DIR__ . '/../../templates/error.php');
