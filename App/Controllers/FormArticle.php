ac128ecedb0c69294eded25e6c8b0964bb071ef9
mr-kamy <steam-punker@mail.ru>
2018-04-26 10:32:12 +0300

    fixed bag

--- App/Controllers/FormArticle.php
+++ App/Controllers/FormArticle.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 
 use App\Controller;
+use App\Exceptions\Exception404;
 
 class FormArticle extends Controller
 {
@@ -12,9 +13,12 @@ class FormArticle extends Controller
     {
         if ((isset($_GET['id'])) && ($_GET['id'] != '')) {
             $id = $_GET['id'];
-
-            $this->view->article = \App\Models\Article::findById($id);
-            echo $this->view->render(__DIR__ . '/../../templates/updateArticle.php');
+            if(\App\Models\Article::findById($id)){
+                $this->view->article = \App\Models\Article::findById($id);
+                echo $this->view->render(__DIR__ . '/../../templates/updateArticle.php');
+            } else {
+                throw new Exception404('Новость не найдена', 404);
+            }
         } else {
             echo $this->view->render(__DIR__ . '/../../templates/appendArticle.php');
         }
