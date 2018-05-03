1a2eac34d475f7500bd355f87470affcc3a2dcf0
mr-kamy <steam-punker@mail.ru>
2018-05-03 15:20:35 +0300

    + AdminDataTable

--- public/index.php
+++ public/index.php
@@ -2,32 +2,6 @@
 
 require __DIR__ . '/../App/autoload.php';
 
-/*
-$articles = \App\Models\Article::findAll();
-
-$functions = [
-    function ($article) {
-        return $article->id;
-    },
-    function ($article) {
-        return $article->title;
-    },
-    function ($article) {
-        return $article->content;
-    },
-    function ($article) {
-        if (isset($article->author)) {
-            return $article->author->name;
-        } else {
-            return '';
-        }
-    },
-];
-
-$table = new \App\AdminDataTable($articles, $functions);
-$table->render();
-*/
-
 $uri = $_SERVER['REQUEST_URI'];
 $parts = explode('/', $uri);
 
